72642de9d0cabf6e5fcf19af1d8b095ed099035d
Couty SSH <93544260+CoutySSH@users.noreply.github.com>
2023-06-30 14:06:51 -0300

    Update in.sh

--- in.sh
+++ in.sh
@@ -12,11 +12,11 @@ echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read
 clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
 apt-get install figlet -y > /dev/null 2>&1
-rm /bin/pweb > /dev/null 2>&1
+rm /bin/CoutySSH > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/alian87/psshplus/main3/gestorssh/pweb > /dev/null 2>&1
-chmod 777 pweb > /dev/null 2>&1
+wget https://raw.githubusercontent.com/CoutySSH/imagens-png/main/CoutySSH > /dev/null 2>&1
+chmod 777 CoutySSH > /dev/null 2>&1
 clear
 mkdir /bin/ppweb > /dev/null 2>&1
 cd /bin/ppweb || exit
@@ -37,8 +37,9 @@ chmod 777 *.sh > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mDEPENDENCIAS CONCLUÍDA\033[1;33m • \033[0m"
 echo ""
-bash <(curl -sL https://raw.githubusercontent.com/CoutySSH/imagens-png/main/install01.sh)
-#echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
+echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mCoutySSH\033[0m"
+clear
+CoutySSH
 #echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m"
 #cat /dev/null > ~/.bash_history && history -c
 #exit
